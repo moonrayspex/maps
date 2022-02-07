c1cd01e94bb2b0c023aa46ddeadd4c40296c7927
Rachel <89171396+moonrayspex@users.noreply.github.com>
2022-02-07 13:38:38 -0500

    Update nordic.js

--- ski/nordic.js
+++ ski/nordic.js
@@ -3,7 +3,7 @@
 // Use function to create map 
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8.25,
+      zoom: 9,
       center: { lat: 43.874721303468206, lng: -79.73041974777321}, 
       mapTypeId: "terrain"
     });
